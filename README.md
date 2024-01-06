00d913f8044679aa7d7bbc2c05aa6c780488af86
Serukyas <serukyastt@gmail.com>
2024-01-06 21:52:06 +0300

    Website remake 1.0
    Archives old files and put the new files in place

--- README.md
+++
@@ -1,1 +0,0 @@
-To access the website go to https://developers-ethiopia.github.io/
---
+++ script.js
@@ -0,0 +1,8 @@
+// responsive siebar
+let sidebar = document.querySelector(".sideBar")
+function showSideBar() {
+    sidebar.style.display = "flex"
+}
+function hideSideBar() {
+    sidebar.style.display = "none"
+}
